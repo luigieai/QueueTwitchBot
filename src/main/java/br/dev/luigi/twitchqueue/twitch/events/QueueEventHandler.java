725ef822d02295c2930c6dc01e82f3d002c7719a
luigieai <luigimarqs@gmail.com>
2022-08-18 01:57:04 -0300

    :wrench: Added queue add command

--- src/main/java/br/dev/luigi/twitchqueue/twitch/events/QueueEventHandler.java
+++ src/main/java/br/dev/luigi/twitchqueue/twitch/events/QueueEventHandler.java
@@ -7,6 +7,7 @@ import br.dev.luigi.twitchqueue.twitch.TwitchBot;
 import br.dev.luigi.twitchqueue.twitch.TwitchConfiguration;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.github.twitch4j.common.enums.CommandPermission;
 import com.github.twitch4j.pubsub.domain.ChannelPointsReward;
 import com.github.twitch4j.pubsub.events.RewardRedeemedEvent;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class QueueEventHandler {
@@ -36,6 +38,7 @@ public class QueueEventHandler {
     private void handleMessageEvent(ChannelMessageEvent channelMessageEvent) {
         String message = channelMessageEvent.getMessage();
         if(!message.startsWith("!")) return;
+
         if(message.equalsIgnoreCase("!queue")){
             List<QueuedUser> users = queuedService.getAllQueuedUsers();
             if(users.isEmpty()){
@@ -47,6 +50,30 @@ public class QueueEventHandler {
             sb.append("Queue: ");
             users.forEach(user -> sb.append( user.getUsername() + " " ));
             twitchClient.getChat().sendMessage(twitchConfiguration.getChannelName(), sb.toString());
+        }
+
+        if(message.startsWith("!queue add")) {
+            String user = channelMessageEvent.getUser().getName();
+            Set<CommandPermission> userPermissions = channelMessageEvent.getPermissions();
+            boolean havePermission = userPermissions.stream()
+                    .anyMatch(permission -> permission.name().equals("MODERATOR"));
+            if(!havePermission){
+                TwitchqueueApplication.log.info(user + " don't have permission to manually add user at queue");
+                return;
+            }
+            String userToAdd = message.split(" ", 3)[2];
+            if(queuedService.isUserQueued(userToAdd)){
+                TwitchqueueApplication.log.info("Moderator " + user + " tried to add " + userToAdd +
+                        " to queue but the user is already in queue");
+                twitchClient.getChat().sendMessage(twitchConfiguration.getChannelName(),
+                        "@"+ user + " the user is already in queue!");
+                return;
+            }
+            TwitchqueueApplication.log.info("Moderator " + user + " Issued manual addition of the user " + userToAdd +
+                    " in queue");
+            queuedService.addQueuedUser(userToAdd);
+            twitchClient.getChat().sendMessage(twitchConfiguration.getChannelName(),
+                    "@" + userToAdd + " had been added to the queue to play by " + user);
         }
 
     }
